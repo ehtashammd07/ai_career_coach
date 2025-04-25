060c1d5c473a8b8caedc86dab28ada28629929d8
ehtashammd07 <mdehtasham165@gmail.com>
2025-04-25 11:34:32 +0530

    env update

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗💗 by ehtashammd07</p>
+                <p>Made with 💗 by ehtashammd07</p>
               </div>
             </footer>
           </ThemeProvider>
